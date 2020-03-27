d3f53ba90d3ea2120aa2b1c9f71f7099eb74a6d9
econrardy <emmanuel.conrardy@arolla.fr>
2020-03-27 08:52:50 +0100

    init

---
+++ SandBox.Test/MyTest.cs
@@ -0,0 +1,21 @@
+using System.Linq;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Playground.Test
+{
+    [TestClass]
+    public class MyTest
+    {
+        [TestMethod]
+        public void WhatShouldITest()
+        {
+            //arrange
+            
+            //act
+
+            //assert
+        }
+    }
+}
