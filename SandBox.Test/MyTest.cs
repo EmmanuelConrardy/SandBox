41fe9e2eb00067a620fd0b1620066d14c2a2b93e
econrardy <emmanuel.conrardy@arolla.fr>
2020-03-27 14:41:29 +0100

    Add test WizardUseStupefix

--- SandBox.Test/MyTest.cs
+++ SandBox.Test/MyTest.cs
@@ -25,6 +25,42 @@ namespace Playground.Test
             //assert
             Assert.AreEqual(house, wizard.House);
         }
+
+         [TestMethod]
+        public void WizardUseStupefix()
+        {
+            //arrange
+            var harry = new Wizard("Harry Potter");
+            harry.Learn(new Spell("stupefix","stun",1));
+            var albus = new Wizard("Albus Dumbledore");
+            
+            //act
+            harry.Use("stupefix").On(albus);
+
+            //assert
+            Assert.AreEqual(9,albus.Health);
+            Assert.AreEqual("stun",albus.State);
+        }
+    }
+
+    internal class Spell
+    {
+        public string Name;
+        private string state;
+        private int damage;
+
+        public Spell(string name, string state, int damage)
+        {
+            this.Name = name;
+            this.state = state;
+            this.damage = damage;
+        }
+
+        internal void On(Wizard wizard)
+        {
+            wizard.Health -= this.damage;
+            wizard.State = this.state;
+        }
     }
 
     public class Wizard
@@ -34,13 +70,27 @@ namespace Playground.Test
         public Wizard(string name)
         {
             this.name = name;
+            this.Health = 10;
         }
 
         public House House { get; internal set; }
+        public int Health { get; internal set; }
+        public string State { get; internal set; }
+
+        private List<Spell> spells = new List<Spell>();
+        internal void Learn(Spell spell)
+        {
+            spells.Add(spell);
+        }
 
         internal void SelectHouse(House house)
         {
             this.House = house;
+        }
+
+        internal Spell Use(string spellName)
+        {
+            return spells.Find(s => s.Name == spellName);
         }
     }
 
